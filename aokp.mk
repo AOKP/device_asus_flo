ac28a95b8eee54ca4f8d0116d4eea080d253411e
STELIX <ssspinni@gmail.com>
2015-10-15 19:32:15 +0200

    Flo - AOKP [1/2]
    Change-Id: I330b341b8d977f44b55e34020a5c1281b227e7e5
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- aokp.mk
+++ aokp.mk
@@ -1,21 +1,21 @@
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
-TARGET_SCREEN_WIDTH := 1200
-TARGET_BOOTANIMATION_HALF_RES := true
+TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOTANIMATION_HALF_RES := false
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+# Inherit some common AOKP stuff.
+$(call inherit-product, vendor/aokp/configs/common_full_tablet_wifionly.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/aokp/configs/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/asus/flo/full_flo.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := flo
-PRODUCT_NAME := cm_flo
+PRODUCT_NAME := aokp_flo
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := asus
